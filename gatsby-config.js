fb4ae20dafebea979159b5092b06d229635e4619
JSMarsh813 <janetspellman13@gmail.com>
2023-12-30 21:25:39 -0800

    added async mode for gatsby omni font loader to help with loading time

--- gatsby-config.js
+++ gatsby-config.js
@@ -52,6 +52,7 @@ module.exports = {
     {
       resolve: `gatsby-omni-font-loader`,
       options: {
+        mode: "async",
         enableListener: true,
         preconnect: [
           `https://fonts.googleapis.com`,
